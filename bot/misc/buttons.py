5705de44cb86bff685cb0de7753c89624248a35f
Ilya Shmyrev <shmyryv@yandex.ru>
2023-04-23 15:28:51 +0500

    Made buttons.py more beautiful

--- bot/misc/buttons.py
+++ bot/misc/buttons.py
@@ -1,9 +1,9 @@
 import disnake
 
 
-class Question(disnake.ui.View):
-    def __init__(self, interaction: disnake.Interaction, function):
-        super().__init__(timeout=300)
+class Buttons(disnake.ui.View):
+    def __init__(self, interaction, function, timeout: int = 300):
+        super().__init__(timeout=timeout)
         self.interaction = interaction
         self.userID = interaction.user.id
         self.function = function
@@ -16,45 +16,35 @@ class Question(disnake.ui.View):
     async def on_timeout(self):
         await self.disableAllItems()
 
+
+class Question(Buttons):
+    def __init__(self, interaction: disnake.CommandInteraction, function):
+        super().__init__(interaction, function)
+
     @disnake.ui.button(label="Да", style=disnake.ButtonStyle.green)
-    async def yesButton(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def yesButton(self, button: disnake.ui.Button, interaction: disnake.CommandInteraction):
         if interaction.user.id != self.userID:
-            # noinspection PyUnresolvedReferences
             await interaction.response.send_message(content=f"Это кнопка не для вас", ephemeral=True)
             return
         await self.disableAllItems()
         await self.function(interaction)
 
     @disnake.ui.button(label="Нет", style=disnake.ButtonStyle.red)
-    async def noButton(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def noButton(self, button: disnake.ui.Button, interaction: disnake.CommandInteraction):
         if interaction.user.id != self.userID:
-            # noinspection PyUnresolvedReferences
             await interaction.response.send_message(content=f"Это кнопка не для вас", ephemeral=True)
             return
         await self.disableAllItems()
-        # noinspection PyUnresolvedReferences
         await interaction.response.send_message(content=f"Отменено")
 
 
-class Continue(disnake.ui.View):
-    def __init__(self, interaction: disnake.Interaction, function):
-        super().__init__(timeout=300)
-        self.interaction = interaction
-        self.userID = interaction.user.id
-        self.function = function
-
-    async def disableAllItems(self):
-        for item in self.children:
-            item.disabled = True
-        await self.interaction.edit_original_response(view=self)
-
-    async def on_timeout(self):
-        await self.disableAllItems()
+class Continue(Buttons):
+    def __init__(self, interaction: disnake.CommandInteraction, function):
+        super().__init__(interaction, function)
 
     @disnake.ui.button(label="Продолжить", style=disnake.ButtonStyle.blurple)
-    async def continueButton(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def continueButton(self, button: disnake.ui.Button, interaction: disnake.CommandInteraction):
         if interaction.user.id != self.userID:
-            # noinspection PyUnresolvedReferences
             await interaction.response.send_message(content=f"Это кнопка не для вас", ephemeral=True)
             return
         await self.function(interaction)
